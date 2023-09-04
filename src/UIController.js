02acfade9e4b94b9cac1c126a1396b21af6985c7
Siriusxz <114768817+Siriuszx@users.noreply.github.com>
2023-09-04 20:22:54 +0200

    Update task look, add new screen task info

--- src/UIController.js
+++ src/UIController.js
@@ -6,7 +6,71 @@ class UIController {
     #addNewTaskEl(taskObj) {
         const newTaskEl = document.createElement('li');
         newTaskEl.classList.add('task-item');
-        newTaskEl.textContent = taskObj.title;
+
+
+        const taskTitle = document.createElement('h3');
+        taskTitle.classList.add('item-title');
+        taskTitle.textContent = taskObj.title;
+
+        // Info container(<ul>)
+        const taskInfoContainer = document.createElement('ul');
+        taskInfoContainer.classList.add('item-info');
+        
+
+        // Info Item: Description
+        const infoItem1 = document.createElement('li');
+        infoItem1.classList.add('info-item');
+
+        const infoDescTitle = document.createElement('span');
+        infoDescTitle.classList.add('info-title');
+        infoDescTitle.textContent = 'Description: ';
+
+        const infoDescData = document.createElement('span');
+        infoDescData.classList.add('info-data', 'info-desc');
+        infoDescData.textContent = taskObj.description;
+
+        infoItem1.appendChild(infoDescTitle);
+        infoItem1.appendChild(infoDescData);
+
+        // Info Item: Due date
+        const infoItem2 = document.createElement('li');
+        infoItem2.classList.add('info-item');
+
+        const infoDueTitle = document.createElement('span');
+        infoDueTitle.classList.add('info-title');
+        infoDueTitle.textContent = 'Due date: ';
+
+        const infoDueDate = document.createElement('span');
+        infoDueDate.classList.add('info-data', 'info-due-date');
+        infoDueDate.textContent = taskObj.dueDate;
+
+        infoItem2.appendChild(infoDueTitle);
+        infoItem2.appendChild(infoDueDate);
+
+        // Info Item: Priority
+        const infoItem3 = document.createElement('li');
+        infoItem3.classList.add('info-item');
+
+        const infoPrioTitle = document.createElement('span');
+        infoPrioTitle.classList.add('info-title');
+        infoPrioTitle.textContent = 'Priority: ';
+
+        const infoPrioData = document.createElement('span');
+        infoPrioData.classList.add('info-data', 'info-priority');
+        infoPrioData.textContent = taskObj.priority;
+
+        infoItem3.appendChild(infoPrioTitle);
+        infoItem3.appendChild(infoPrioData);
+
+
+        taskInfoContainer.appendChild(infoItem1);
+        taskInfoContainer.appendChild(infoItem2);
+        taskInfoContainer.appendChild(infoItem3);
+
+
+        newTaskEl.appendChild(taskTitle);
+        newTaskEl.appendChild(taskInfoContainer);
+
 
         this.todoContainer.appendChild(newTaskEl);
     }
@@ -16,11 +80,11 @@ class UIController {
 
         taskArr.forEach((taskObj) => {
             this.#addNewTaskEl(taskObj);
-         });
+        });
     }
 
-     // For debugging purposes
-     logTask(taskObj) {
+    // For debugging purposes
+    logTask(taskObj) {
         console.log(`Title: ${taskObj.title}
 Description: ${taskObj.description}
 Due Date: ${taskObj.dueDate}
