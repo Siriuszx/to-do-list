662df5e1cb6dbefdfd4d02707786f1e256da2b43
Siriusxz <114768817+Siriuszx@users.noreply.github.com>
2023-09-09 20:03:14 +0200

    Update task style

--- src/index.js
+++ src/index.js
@@ -6,25 +6,29 @@ const taskCollection = [
         title: 'CS HW', 
         description: 'Revise graph theory', 
         dueDate: '01.01.2024', 
-        priority: 2
+        priority: 2,
+        taskGroup: 'inbox',
     },
     {
         title: 'Math HW', 
         description: 'Linear Algebra HW', 
         dueDate: '21.04.2024', 
-        priority: 2
+        priority: 2,
+        taskGroup: 'inbox',
     },
     {
         title: 'Gym', 
         description: 'Back workout', 
         dueDate: '11.11.2023', 
-        priority: 2
+        priority: 2,
+        taskGroup: 'inbox',
     },
     {
         title: 'German', 
         description: 'Lektion 8', 
         dueDate: '13.05.2021', 
-        priority: 1
+        priority: 1,
+        taskGroup: 'inbox',
     },
 ];
 
@@ -35,4 +39,5 @@ todoList.updateListeners();
 // todoList.addNewTask(taskCollection[1]);
 // todoList.addNewTask(taskCollection[2]);
 // todoList.addNewTask(taskCollection[3]);
+
 
